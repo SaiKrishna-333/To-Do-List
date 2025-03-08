577b21a66da476b0a23df743ba4307c3e04e00d2
Sai Krishna S <saikrishnas.yadav@gmail.com>
2025-03-08 13:44:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-let listContainer = document.getElementById('list-container')
-let head = document.querySelector('head')
-let inputBox = document.getElementById('input-box')
+ let listContainer = document.getElementById('list-container');
+    let inputBox = document.getElementById('input-box');
 
+    function addTask() {
+        if (inputBox.value === '') {
+            alert('Please Enter the Text');
+        } else {
+            let task = document.createElement('li');
+            task.textContent = inputBox.value;
+            listContainer.appendChild(task);
 
-function addTask(){
-    if(inputBox.value == ''){
-        alert('Please Enter the Text')
+            let span = document.createElement('span');
+            span.textContent = "\u00d7";
+            task.appendChild(span);
+            
+            span.addEventListener('click', () => {
+                task.remove();
+                saveData();
+            });
+        }
+        inputBox.value = '';
+        saveData();
     }
-    else{
-        const task = document.createElement('li')
-        task.textContent = inputBox.value;
-        listContainer.appendChild(task)
-        // inputBox.value = ''
-        let span = document.createElement('span')
-        span.textContent = "\u00d7"
-        task.appendChild(span)
-        span.style.right = '0px';
+
+    listContainer.addEventListener('click', (e) => {
+        if (e.target.tagName === 'LI') {
+            e.target.classList.toggle('checked');
+            saveData();
+        }
+    });
+
+    function saveData() {
+        localStorage.setItem('data', listContainer.innerHTML);
     }
-    inputBox.value = '';
-    saveData()
-}
 
-listContainer.addEventListener('click', (e)=>{
-    if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked")
-        saveData()
+    function showTask() {
+        listContainer.innerHTML = localStorage.getItem('data');
     }
-    else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove()
-        saveData()
-    }
-})
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML)
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-
-showTask()
+    showTask();
